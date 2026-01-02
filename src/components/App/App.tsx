896197a3087bec6dfdf6bc4a5f7f74220855db68
Serhii Repinskyi <repinskyisv@gmail.com>
2026-01-02 22:41:08 +0200

    feat: calculate total votes and positive rate

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -21,11 +21,23 @@ const App = () => {
     setVotes({ good: 0, neutral: 0, bad: 0 });
   };
 
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
   return (
     <div className={css.app}>
       <CafeInfo />
+
       <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
-      <VoteStats votes={votes} totalVotes={0} positiveRate={0} />
+
+      <VoteStats
+        votes={votes}
+        totalVotes={totalVotes}
+        positiveRate={positiveRate}
+      />
     </div>
   );
 };
