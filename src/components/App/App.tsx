4e299126099b3bed0045be0c7137b61007375e65
Serhii Repinskyi <repinskyisv@gmail.com>
2026-01-06 19:07:51 +0200

    feat: show Reset button only when votes exist

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -32,7 +32,11 @@ const App = () => {
     <div className={css.app}>
       <CafeInfo />
 
-      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
 
       {totalVotes > 0 ? (
         <VoteStats
